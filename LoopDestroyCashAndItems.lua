9d23190b8f4e25840a488cf4e0ae9f2b1767aa55
Jordan / 15rih <129924494+15rih@users.noreply.github.com>
2023-04-05 12:22:16 -0700

    Create LoopDestroyCashAndItems.lua

---
+++ LoopDestroyCashAndItems.lua
@@ -0,0 +1,19 @@
+local jordan1 = workspace.Ignored.Drop
+local jordan11 = workspace.Ignored.ItemsDrop
+local jordan2 = "MoneyDrop"
+
+getgenv().LoopDel = true
+while LoopDel ~= false do
+    coroutine.resume(coroutine.create(function()
+        for i,v in pairs(jordan1:GetChildren()) do
+            if v.Name == jordan2 or v:isA("Part") or v:IsA("MeshPart") then
+                v:Destroy()
+                for i,e in ipairs(jordan11:GetChildren()) do
+                    if e:IsA("Part") then
+                        e:Destroy()
+                    end
+                end
+            end
+        end
+    end))
+end
